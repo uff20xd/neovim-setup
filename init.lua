4b051fe5049186a34bc7876da2d2b91983346f51
uff20xd <donut121008@gmail.com>
2025-06-15 19:30:30 +0200

    k.

--- init.lua
+++ init.lua
@@ -1,6 +1,10 @@
 local home_dir = vim.env.HOME;
 print(home_dir)
-local custom_path = home_dir .. "/.config/nvim/?.lua;" .. home_dir .. "/.config/nvim/?/init.lua;./?.lua;./?/init.lua"
+
+local custom_path = home_dir .. "/.config/nvim/?.lua;" .. home_dir .. "/.config/nvim/?/init.lua;./?.lua;./?/init.lua;" .. home_dir .. "/.config/nvim/plugin/?.lua"
+package.path = package.path .. ";" .. custom_path
+
+local custom_path = home_dir .. "/.config/nvim/plugin/?.lua;" .. home_dir .. "/.config/nvim/plugin/?.lua"
 package.path = package.path .. ";" .. custom_path
 
 local custom_path = home_dir .. "/.config/nvim/?.so"
