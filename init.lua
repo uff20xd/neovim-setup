4c54d2b5f04ca978c80f904acb3a195921a1a938
uff20xd <donut121008@gmail.com>
2025-06-15 20:24:30 +0200

    k.

--- init.lua
+++ init.lua
@@ -1,17 +1,19 @@
 local home_dir = vim.env.HOME;
 
-local custom_path = home_dir .. "/.config/nvim/?.lua;" .. home_dir .. "/.config/nvim/?/init.lua;?.lua;?/init.lua" 
-package.path = package.path .. ";" .. custom_path
+vim.opt.runtimepath:append(vim.fn.stdpath("config") .. "/pack/treesitter/start/nvim-treesitter")
 
-local custom_path = home_dir .. "/.config/nvim/plugin/?.lua;" .. home_dir .. "/.config/nvim/plugin/?/init.lua"
-package.path = package.path .. ";" .. custom_path
+--local custom_path = home_dir .. "/.config/nvim/?.lua;" .. home_dir .. "/.config/nvim/?/init.lua;?.lua;?/init.lua" 
+--package.path = package.path .. ";" .. custom_path
 
---local custom_path = home_dir .. "/.config/nvim/plugin/?/lua/?.lua;" .. home_dir .. "/.config/nvim/plugin/?/plugin/init.lua"
+--local custom_path = home_dir .. "/.config/nvim/plugin/?.lua;" .. home_dir .. "/.config/nvim/plugin/?/init.lua"
+--package.path = package.path .. ";" .. custom_path
+
+--local custom_path = home_dir .. "/.config/nvim/plugin/nvim-treesitter/lua/?.lua" 
 --package.path = package.path .. ";" .. custom_path
 
 --local custom_path = home_dir .. "/.config/nvim/plugin/?/plugin/?.lua"
 --package.path = package.path .. ";" .. custom_path
---
+
 --local custom_path = home_dir .. "/.config/nvim/?.so"
 --package.cpath = package.path .. ";" .. custom_path
 
