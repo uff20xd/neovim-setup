01466715113f8061f8ba39d76364d5b99e2e103c
uff20xd <donut121008@gmail.com>
2025-06-15 19:02:48 +0200

    k.

--- init.lua
+++ init.lua
@@ -1,1 +1,9 @@
-print("Welcome to Badvim")
+local custom_path = "~/.config/nvim/?.lua;~/.config/nvim/?/init.lua"
+package.path = package.path .. ";" .. custom_path
+
+local custom_path = "~/.config/nvim/?.so"
+package.cpath = package.path .. ";" .. custom_path
+test = function() 
+	print("works")
+end
+require("after.config")
