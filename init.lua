66a2e35247a5a2a2fd17794702da1aa9e5cd94ea
uff20xd <donut121008@gmail.com>
2025-06-15 20:26:53 +0200

    k.

--- init.lua
+++ init.lua
@@ -1,9 +1,11 @@
 local home_dir = vim.env.HOME;
 
-vim.opt.runtimepath:append(vim.fn.stdpath("config") .. "/pack/treesitter/start/nvim-treesitter")
+local test = vim.fn.stdpath("config") .. "/pack/treesitter/start/nvim-treesitter"
+print(test)
+vim.opt.runtimepath:append(test)
 
---local custom_path = home_dir .. "/.config/nvim/?.lua;" .. home_dir .. "/.config/nvim/?/init.lua;?.lua;?/init.lua" 
---package.path = package.path .. ";" .. custom_path
+local custom_path = home_dir .. "/.config/nvim/?.lua;" .. home_dir .. "/.config/nvim/?/init.lua;?.lua;?/init.lua" 
+package.path = package.path .. ";" .. custom_path
 
 --local custom_path = home_dir .. "/.config/nvim/plugin/?.lua;" .. home_dir .. "/.config/nvim/plugin/?/init.lua"
 --package.path = package.path .. ";" .. custom_path
