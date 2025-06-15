0ecf1fd87f46c21de5f09577dbe86a6e8f0f54bf
uff20xd <donut121008@gmail.com>
2025-06-15 19:20:25 +0200

    k.

--- copy.sh
+++ copy.sh
@@ -1,3 +1,4 @@
 rm -r ~/.config/nvim
 cp -rp ~/programming/configuration/setup/neovim/no_plug/. ~/.config/nvim
+rm -rf ~/.config/nvim/.git
 
--- init.lua
+++ init.lua
@@ -1,9 +1,11 @@
-local custom_path = "~/.config/nvim/?.lua;~/.config/nvim/?/init.lua"
+local home_dir = vim.env.HOME;
+print(home_dir)
+local custom_path = home_dir .. "/.config/nvim/?.lua;" .. home_dir .. "/.config/nvim/?/init.lua;./?.lua;./?/init.lua"
 package.path = package.path .. ";" .. custom_path
 
-local custom_path = "~/.config/nvim/?.so"
+local custom_path = home_dir .. "/.config/nvim/?.so"
 package.cpath = package.path .. ";" .. custom_path
-test = function() 
-	print("works")
-end
+
+
 require("after.config")
+require("after.keymap")
