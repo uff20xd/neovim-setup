eafb7f18858dc29774d53f87d750788a9fadb9bc
uff20xd <donut121008@gmail.com>
2025-06-15 21:52:51 +0200

    k

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,6 +1,6 @@
-vim.keymap.set('n', '<leader>ff', ":Telescope find_files <CR>", { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', ":Telescope git_files<CR>", { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', ":Telescope current_buffer_fuzzy_find<CR>", { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', ":Telescope buffers<CR>", { desc = 'Telescope help tags' })
-vim.keymap.set('n', '<leader>fs', ":Telescope grep_string<CR>", { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>ff', ":Telescope find_files <CR>", { silent = true, noremap = true })
+vim.keymap.set('n', '<leader>fg', ":Telescope git_files<CR>", { silent = true, noremap = true })
+vim.keymap.set('n', '<leader>fb', ":Telescope current_buffer_fuzzy_find<CR>", { silent = true, noremap = true })
+vim.keymap.set('n', '<leader>fh', ":Telescope buffers<CR>", { silent = true, noremap = true })
+vim.keymap.set('n', '<leader>fs', ":Telescope grep_string<CR>", { silent = true, noremap = true })
 
