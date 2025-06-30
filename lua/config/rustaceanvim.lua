f0ee7bb77ceef2959ec25e839387ac332ef69e9d
uff20xd <donut121008@gmail.com>
2025-06-30 13:54:29 +0200

    k

--- lua/config/rustaceanvim.lua
+++ lua/config/rustaceanvim.lua
@@ -16,3 +16,23 @@ vim.keymap.set(
   end,
   { silent = true, buffer = bufnr }
 )
+
+vim.g.rustaceanvim = {
+  -- Plugin configuration
+  tools = {
+  },
+  -- LSP configuration
+  server = {
+    on_attach = function(client, bufnr)
+      -- you can also put keymaps in here
+    end,
+    default_settings = {
+      -- rust-analyzer language server configuration
+      ['rust-analyzer'] = {
+      },
+    },
+  },
+  -- DAP configuration
+  dap = {
+  },
+}
