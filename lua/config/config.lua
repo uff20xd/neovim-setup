429f9c73c88e23c78fb4969c4d43c633185eff40
uff20xd <donut121008@gmail.com>
2025-07-11 12:45:24 +0200

    k

--- lua/config/config.lua
+++ lua/config/config.lua
@@ -1,24 +1,59 @@
 vim.opt.encoding = 'utf-8'          
+
+-- Basic
+vim.opt.undofile = true
+vim.opt.undolevels = 10000
+vim.opt.undodir = vim.fn.expand("~/.vim/undodir")
+
+vim.opt.syntax = "on"
+vim.opt.number = true               
+vim.opt.relativenumber = true       
+vim.opt.scrolloff = 11
+
+-- Indent
+vim.opt.breakindent = true            
+vim.opt.autoindent = true
+--vim.opt.smarttab = true            
+vim.opt.expandtab = true            
+vim.opt.smarttab = true            
+vim.opt.linebreak = true       
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 
-vim.opt.undofile = true            
-vim.opt.undolevels = 10000
 
-vim.opt.number = true               
-vim.opt.relativenumber = true       
-vim.opt.linebreak = true       
-vim.opt.expandtab = true            
-vim.opt.breakindent = true            
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.wildmode = "full"
+vim.opt.wildmenu = true
+
+-- Miscelaneas (too lazy to google the spelling)
+vim.opt.termguicolors = true
 vim.opt.cursorline = true            
-vim.opt.scrolloff = 8
-vim.opt.smarttab = true            
-vim.opt.conceallevel = 1
-vim.opt.smarttab = true            
+vim.opt.conceallevel = 0
 vim.opt.ruler = true            
-vim.opt.wrap = true
+vim.opt.wrap = false
+vim.opt.signcolumn = "yes"
+vim.opt.showmode = false
+vim.opt.lazyredraw = true
+vim.opt.cmdheight = 1
+vim.opt.showmatch = true
+
+-- File Things
+vim.opt.swapfile = false
+vim.opt.path:append("**")
+vim.opt.modifiable = true
+vim.opt.autoread = true
+vim.opt.autowrite = false
+
+-- Optimizations
+vim.opt.updatetime = 300
+vim.opt.timeoutlen = 500
+
+-- Compilation
 vim.opt.makeprg = "rustc build.rs && ./build"
 
-vim.cmd("syntax on")
-vim.cmd("set clipboard=\"unnamedplus\"")
+-- Clipboard
+vim.opt.clipboard:append("unnamedplus")
--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -9,16 +9,23 @@ vim.keymap.set('n', '<leader>w', ':w<CR>', { noremap = true, silent = true })  -
 vim.keymap.set('n', '<leader>q', ':q<CR>', { noremap = true, silent = true })  -- Quit Neovim
 vim.keymap.set('n', '<leader>e', ':e .<CR>', { noremap = true, silent = true })  -- Open file explorer
 vim.keymap.set('n', '<leader>t', ':terminal <CR>', { noremap = true, silent = true })  -- Open file explorer
-vim.keymap.set('n', '<leader>bf', ':b ', { noremap = true, silent = false})  -- Open file explorer
-vim.keymap.set('n', '<leader>bd', ':bdelete <CR>', { noremap = true, silent = true})  -- Open file explorer
-vim.keymap.set('n', '<leader>ba', ':bnew ', { noremap = true, silent = false})  -- Open file explorer
-vim.keymap.set('n', '<M-h>', ':wincmd h<CR> ', { noremap = true, silent = true})  -- Open file explorer
-vim.keymap.set('n', '<M-j>', ':wincmd j<CR> ', { noremap = true, silent = true})  -- Open file explorer
-vim.keymap.set('n', '<M-k>', ':wincmd k<CR> ', { noremap = true, silent = true})  -- Open file explorer
-vim.keymap.set('n', '<M-l>', ':wincmd l<CR> ', { noremap = true, silent = true})  -- Open file explorer
+vim.keymap.set('n', '<leader>bf', ':b ', { noremap = true, silent = false})  -- Maybe finds a buffer
+vim.keymap.set('n', '<leader>bd', ':bdelete <CR>', { noremap = true, silent = true})  -- Closes a buffer
+vim.keymap.set('n', '<leader>ba', ':bnew ', { noremap = true, silent = false})  -- Adds a new buffer
+vim.keymap.set('n', '<M-h>', ':wincmd h<CR> ', { noremap = true, silent = true})  -- Moving between windows
+vim.keymap.set('n', '<M-j>', ':wincmd j<CR> ', { noremap = true, silent = true})  -- Moving between windows
+vim.keymap.set('n', '<M-k>', ':wincmd k<CR> ', { noremap = true, silent = true})  -- Moving between windows
+vim.keymap.set('n', '<M-l>', ':wincmd l<CR> ', { noremap = true, silent = true})  -- Moving between windows
 
-vim.keymap.set('n', '<Tab>', ':bnext<CR> ', { noremap = true, silent = true})  -- goes forward a buffer
-vim.keymap.set('n', '<S-Tab>', ':bprevious<CR> ', { noremap = true, silent = true})  -- goes backwards a buffer
+vim.keymap.set('n', '<M-k>', ':wincmd k<CR> ', { noremap = true, silent = true})  -- Moving between windows
+vim.keymap.set('n', '<M-l>', ':wincmd l<CR> ', { noremap = true, silent = true})  -- Moving between windows
+
+vim.keymap.set('n', '<leader>sv', ':vsplit<CR> ', { noremap = true, silent = true})  -- Splits vertically
+vim.keymap.set('n', '<leader>sh', ':split<CR> ', { noremap = true, silent = true})  -- Splits horizontally
+vim.keymap.set('n', '<leader>sd', ':wincmd q<CR> ', { noremap = true, silent = true})  -- Closes split
+
+
+vim.keymap.set('n', 'Y', 'y$', { noremap = true, silent = true})  -- Yanks rest of the line
 
 
 --insert mode keybinds
