cf6e9db7381f3d2ba1620841fda56a03f676a3f9
uff20xd <donut121008@gmail.com>
2025-07-11 13:30:29 +0200

    k

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -20,9 +20,12 @@ vim.keymap.set('n', '<M-l>', ':wincmd l<CR> ', { noremap = true, silent = true})
 vim.keymap.set('n', '<M-k>', ':wincmd k<CR> ', { noremap = true, silent = true})  -- Moving between windows
 vim.keymap.set('n', '<M-l>', ':wincmd l<CR> ', { noremap = true, silent = true})  -- Moving between windows
 
+vim.keymap.set('n', '<Tab>', ':bnext<CR> ', { noremap = true, silent = true})  -- Moving between buffer
+vim.keymap.set('n', '<S-Tab>', ':bprevious<CR> ', { noremap = true, silent = true})  -- Moving between buffer
+
 vim.keymap.set('n', '<leader>sv', ':vsplit<CR> ', { noremap = true, silent = true})  -- Splits vertically
 vim.keymap.set('n', '<leader>sh', ':split<CR> ', { noremap = true, silent = true})  -- Splits horizontally
-vim.keymap.set('n', '<leader>sd', ':wincmd q<CR> ', { noremap = true, silent = true})  -- Closes split
+vim.keymap.set('n', '<leader>sd', ':wincmd q<CR> ', { noremap = true, silent = true})  -- closes split
 
 
 vim.keymap.set('n', 'Y', 'y$', { noremap = true, silent = true})  -- Yanks rest of the line
