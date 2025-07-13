3075a921542bf9da11c43f9a6f4c90229369ad2a
uff20xd <donut121008@gmail.com>
2025-07-13 19:33:02 +0200

    k

--- copy.sh
+++ copy.sh
@@ -1,4 +1,4 @@
 rm -r ~/.config/nvim
-cp -rp ~/programming/configuration/setup/neovim/no_manager/. ~/.config/nvim
+cp -rp ~/programming/configuration/neovim/. ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
