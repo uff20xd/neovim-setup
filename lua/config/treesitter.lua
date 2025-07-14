865cfd981d450829c41df4f49d230efd5d58f381
uff20xd <donut121008@gmail.com>
2025-07-14 17:04:57 +0200

    k

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -9,7 +9,7 @@ ts.setup {
     "c", "lua", "python",
     "javascript", "html", "css", "rust", "zig",
     "haskell", "nix", "cpp", "markdown", "commonlisp",
-    "latex",
+    "latex", 
   },
   highlight = {
     enable = true,
