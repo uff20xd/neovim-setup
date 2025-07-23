e2f42912de2d3a1eaba6630687350cf9f93f1d70
uff20xd <donut121008@gmail.com>
2025-07-23 12:48:57 +0200

    k

---
+++ lua/config/mini.lua
@@ -0,0 +1,13 @@
+require('mini.ai').setup()         -- a/i textobjects
+require('mini.align').setup()      -- aligning
+require('mini.bracketed').setup()  -- unimpaired bindings with TS
+require('mini.comment').setup()    -- TS-wise comments
+require('mini.icons').setup()      -- minimal icons
+--require('mini.jump').setup()       -- fFtT work past a line
+require('mini.pairs').setup()      -- pair brackets
+--require('mini.statusline').setup() -- minimal statusline
+require('mini.surround').setup({   -- surround
+  custom_surroundings = {
+    ['l'] = { output = { left = '[', right = ']()'}}
+  }
+})
