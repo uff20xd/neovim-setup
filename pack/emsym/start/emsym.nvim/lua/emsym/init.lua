7819d0be1386d3a87db368252ea07cef64466dd7
uff20xd <donut121008@gmail.com>
2025-10-23 14:39:23 +0200

    k

--- pack/emsym/start/emsym.nvim/lua/emsym/init.lua
+++ pack/emsym/start/emsym.nvim/lua/emsym/init.lua
@@ -1,39 +1,36 @@
-api = vim.api
+local Emsym = {}
 
-win = api.nvim_get_current_win()
+Emsym.catching = {}
 
-print(api.nvim_win_get_cursor(win))
+Emsym.catching["sigma"] = "𝚺"
+Emsym.catching["epsilon"] = "ε"
+Emsym.catching["^1"] = "¹"
+Emsym.catching["^2"] = "²"
+Emsym.catching["^3"] = "³"
+Emsym.catching["^4"] = "⁴"
+Emsym.catching["^5"] = "⁵"
+Emsym.catching["^6"] = "⁶"
+Emsym.catching["^7"] = "⁷"
+Emsym.catching["^8"] = "⁸"
+Emsym.catching["^9"] = "⁹"
+Emsym.catching["^n"] = "ⁿ"
+Emsym.catching["^+"] = "⁺"
+Emsym.catching["^-"] = "⁻"
+Emsym.catching["^i"] = "ⁱ"
+Emsym.catching["sub0"] = "₀"
+Emsym.catching["pi"] = "π"
+Emsym.catching["theta"] = "θ"
 
-catching = {}
-
-catching["sigma"] = "𝚺"
-catching["epsilon"] = "ε"
-catching["^1"] = "¹"
-catching["^2"] = "²"
-catching["^3"] = "³"
-catching["^4"] = "⁴"
-catching["^5"] = "⁵"
-catching["^6"] = "⁶"
-catching["^7"] = "⁷"
-catching["^8"] = "⁸"
-catching["^9"] = "⁹"
-catching["^n"] = "ⁿ"
-catching["^+"] = "⁺"
-catching["^-"] = "⁻"
-catching["^i"] = "ⁱ"
-catching["sub0"] = "₀"
-catching["pi"] = "π"
-catching["theta"] = "θ"
-
-catcher = function()
-  win = api.nvim_get_current_win()
-  buf = api.nvim_get_current_buf()
-  line = api.nvim_get_current_line()
-  pos = api.nvim_win_get_cursor(win)
-  pointer = pos[2] + 1
+Emsym.catcher = function()
+  local api = vim.api
+  local win = api.nvim_get_current_win()
+  local buf = api.nvim_get_current_buf()
+  local line = api.nvim_get_current_line()
+  local pos = api.nvim_win_get_cursor(win)
+  local pointer = pos[2] + 1
   while(true) do
-    if catching[string.sub(line, pointer, pos[2] + 1)] ~= Nil then
-      api.nvim_buf_set_text(buf, pos[1] - 1, pointer - 1, pos[1] - 1, pos[2] + 1, {catching[string.sub(line, pointer, pos[2] + 1)]})
+    if Emsym.catching[string.sub(line, pointer, pos[2] + 1)] ~= Nil then
+      api.nvim_buf_set_text(buf, pos[1] - 1, pointer - 1, pos[1] - 1, pos[2] + 1, {Emsym.catching[string.sub(line, pointer, pos[2] + 1)]})
       break
     end
     pointer = pointer - 1
@@ -43,6 +40,7 @@ catcher = function()
   end
 end
 
-vim.keymap.set('n', '<leader>ms', catcher, { noremap = true, silent = true})
-vim.keymap.set('i', '<C-b>', catcher, { noremap = true, silent = true})
+vim.keymap.set('n', '<leader>ms', Emsym.catcher, { noremap = true, silent = true})
+--vim.keymap.set('i', '<C-b>', Emsym.catcher, { noremap = true, silent = true})
 
+return Emsym
