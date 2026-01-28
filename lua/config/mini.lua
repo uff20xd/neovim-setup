f0f66b3a0b5d42dcb8d635ae292550e333c768b4
uff20xd <uff20xd@proton.me>
2026-01-28 16:12:59 +0100

    k

--- lua/config/mini.lua
+++ lua/config/mini.lua
@@ -3,11 +3,11 @@ require('mini.align').setup()      -- aligning
 require('mini.bracketed').setup()  -- unimpaired bindings with TS
 require('mini.comment').setup()    -- TS-wise comments
 require('mini.icons').setup()      -- minimal icons
---require('mini.jump').setup()       -- fFtT work past a line
+-- require('mini.jump').setup()       -- fFtT work past a line
 require('mini.pairs').setup()      -- pair brackets
---require('mini.statusline').setup() -- minimal statusline
-require('mini.surround').setup({   -- surround
-  custom_surroundings = {
-    ['l'] = { output = { left = '[', right = ']()'}}
-  }
-})
+-- require('mini.statusline').setup() -- minimal statusline
+-- require('mini.surround').setup({   -- surround
+--   custom_surroundings = {
+--     ['l'] = { output = { left = '[', right = ']()'}}
+--   }
+-- })
